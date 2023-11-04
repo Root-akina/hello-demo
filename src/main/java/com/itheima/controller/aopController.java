4646d479a5173ac5b8fe390a0004b14f16947acd
ROOT <amourzeta@163.com>
2023-11-04 12:04:48 +0800

    hello Linux

---
+++ src/main/java/com/itheima/controller/aopController.java
@@ -0,0 +1,11 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class aopController {
+
+    public void fun01(){
+        System.out.println("github..");
+    }
+}
