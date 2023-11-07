d415b84b2adf077fe4ca7634eb3ae88262f3c608
root <147520338+root-akina@users.noreply.github.com>
2023-11-07 17:20:51 +0800

    day_04-Controller

--- src/main/java/com/itheima/controller/day_04.java
+++ src/main/java/com/itheima/controller/day_04.java
@@ -1,4 +1,15 @@
 package com.itheima.controller;
 
 public class day_04 {
+
+    public static void main(String[] args) {
+        int num = getNum(10);
+    }
+    public static int getNum(int n){
+        int b = 0;
+        for (int i = 0; i < n; i++) {
+            b+=i;
+        }
+        return b;
+    }
 }
